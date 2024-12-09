62c2ac12df01b95cfffcc33272e78c8bde84dfe9
chayan <chayanj39@gmail.com>
2024-12-09 13:27:46 +0530

    Shifting of lines done

--- index.js
+++ index.js
@@ -1,57 +1,109 @@
-import { uniqueWords } from "./text.js";
-
-let n = uniqueWords.length
-
-let line1 = document.querySelector('.line1')
-let line2 = document.querySelector('.line2')
-let line3 = document.querySelector('.line3')
+import { uniqueWords } from "./text.js";  
+let n = uniqueWords.length;
+let line1 = document.querySelector('.line1');
+let line2 = document.querySelector('.line2');
+let line3 = document.querySelector('.line3');
 let inputField = document.querySelector('.input');
 
-let text1 = ""
-let text2 = ""
-let text3 = ""
-const maxchar = 75
-for(let i=0;i<13;i++){
-    let num = Math.floor(Math.random()*n)
+let text1 = "";
+let text2 = "";
+let text3 = "";
+const maxchar = 75;
 
-    let w1=uniqueWords[num] + " "
-    let w2=uniqueWords[(num+1)%n] + " "
-    let w3=uniqueWords[(num+2)%n] + " "
-    if((text1.length + w1.length) <maxchar) text1+=w1
-    if ((text2.length + w2.length) <maxchar) text2 += w2;   
-    if ((text3.length + w3.length) <maxchar) text3 += w3;
+// Generate lines
+for(let i = 0; i < 13; i++){     
+    let num = Math.floor(Math.random() * n);
+    let w1 = uniqueWords[num] + " ";
+    let w2 = uniqueWords[(num+1) % n] + " ";
+    let w3 = uniqueWords[(num+2) % n] + " ";
     
+    if((text1.length + w1.length) < maxchar) text1 += w1;     
+    if((text2.length + w2.length) < maxchar) text2 += w2;        
+    if((text3.length + w3.length) < maxchar) text3 += w3;      
+} 
+
+// Set line texts
+line1.innerText = text1;
+line2.innerText = text2;
+line3.innerText = text3;   
+inputField.focus();  
+
+// Game state variables
+let lines = [text1, text2, text3];
+let lineElements = [line1, line2, line3];
+let currentLineIndex = 0;
+let TotalCorrectTextLength = 0;
+let TotalTypedTextLength = 0;
+let TotalTextLength = 0;
+let onloop = false
+
+
+function getNewText(){
+    let newtext = ""
+    for(let i=0;i<13;i++){
+        let num = Math.floor(Math.random() * n);
+        let w1 = uniqueWords[num] + " ";
+
+        if((newtext.length + w1.length) < maxchar) newtext += w1; 
+        else break;
+    }
+    return newtext;
 }
-line1.innerText = text1
-line2.innerText = text2
-line3.innerText = text3
 
 
-inputField.focus();
 
+inputField.addEventListener('input', (e) => {
+    const typedText = e.target.value;
+    const targetText = lines[currentLineIndex];
+    let updatedText = "";
 
-inputField.addEventListener('input',(e)=>{
-    let typedText = e.target.value; 
-    let targetText = text1; 
-    let lineToCheck = line1
-    let updatedText = ""; 
 
 
     for (let i = 0; i < targetText.length; i++) {
+        const typedLetter = typedText[i];
+        const correctLetter = targetText[i];
 
-        const typedLetter = typedText[i]; 
-        const correctLetter = targetText[i]; 
+
         if (i < typedText.length) {
-            updatedText += (typedLetter === correctLetter
-                ? `<span class="correct">${typedLetter}</span>`
-                : `<span class="incorrect">${correctLetter}</span>`);
+            if (typedLetter === correctLetter) {
+                updatedText += `<span class="correct">${typedLetter}</span>`;
+            } else {
+                updatedText += `<span class="incorrect">${correctLetter}</span>`;
+                
+            }
         } else {
             updatedText += `<span class="default">${correctLetter}</span>`;
+            
         }
+    }
+    lineElements[currentLineIndex].innerHTML = updatedText;
+    if (typedText.length === targetText.length) {
+
+        TotalTypedTextLength += typedText.length;
+        TotalCorrectTextLength += targetText.length;
+        currentLineIndex = (currentLineIndex + 1) % 2;
+        inputField.value = "";
+        if (currentLineIndex === 0) {
+            if(onloop!=true){
+                TotalTextLength += text1.length + text2.length 
+                onloop=true
+            }
+            else TotalTextLength += text2.length
+
+            // removing first line and adding a new
+            lines.shift(); 
+            lines.push(getNewText());
+
+            // rotation
+            lineElements[0].innerHTML = lineElements[1].innerHTML;
+            lineElements[1].innerHTML = lines[1];
+            lineElements[2].innerHTML = lines[2];
+            currentLineIndex = 1;     
+        }
+        inputField.value = "";  
+
 
     }
-    lineToCheck.innerHTML = updatedText;
+}
 
-})
-
-
+);
